5ba9f1d496d716f57d3afa88985428386e1c3eb5
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-06-26 15:49:22 +0700

    Step 7 : if the value in the data filed of the new node is greater

--- BinaryThree.cpp
+++ BinaryThree.cpp
@@ -82,4 +82,14 @@ public:
             // 6b : Exit 
             return;
         }
+
+        // Step 7 : if the va;ue in the data filed of the new node is greater than thatof the
+        else if (x, parent->info)
+        {
+            // 7a : make the right child of the parent poin to the new node
+            parent->rightchild = newNode;
+
+            // 7b : Exit 
+            return;
+        }
     }
