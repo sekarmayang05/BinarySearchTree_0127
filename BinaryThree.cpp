421be7d5723b3f9e3717ad5acd96a08354bfa496
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-06-26 15:52:27 +0700

    menambhakan void inorder

--- BinaryThree.cpp
+++ BinaryThree.cpp
@@ -100,4 +100,17 @@ public:
         return ROOT == nullptr;
     }
 
-   
+    void inorder (Node *ptr)
+    {
+        if (isEmpty())
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr == nullptr)
+            return;
+            
+        inorder(ptr->leftchild);
+        cout << ptr->info << " "; //parent
+        inorder(ptr->rightchild);
+    }
