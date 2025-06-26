1fce6937b08e41f40de78ea20e4b77eeba2c0fda
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-06-26 16:02:06 +0700

    membuat Constructor for the node class

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+
+    Node *leftchild;
+    Node *rightchild;
+
+    //Constructor for the node class
+    Node()
+    {
+        leftchild = nullptr; // intialize left child to null
+        rightchild = nullptr; // intialize right child to null
+    }
+};
